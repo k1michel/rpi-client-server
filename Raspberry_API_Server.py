0c325b105aa17c980b1ec86582f3cafae5393c86
Michel Álvarez <michel_civic@hotmail.com>
2021-04-01 17:49:03 +0200

    Corrigiendo sintaxis diccionarios

--- Raspberry_API_Server.py
+++ Raspberry_API_Server.py
@@ -19,27 +19,30 @@ km_contactor.off()
 contactor_activo= {
     'estado_contactor': bool(False)
 }
+recibir_desactivacion = {
+    'desactivacion': None
+}
 ####################
 # PROGRAMA BUCLE
 ####################
 while True:
-    if pulsador_1.is_pressed==True and contactor_activo.estado_contactor==False:
-        contactor_activo.estado_contactor= True
-    if contactor_activo.estado_contactor==True:
+    if pulsador_1.is_pressed==True and contactor_activo['estado_contactor']==False:
+        contactor_activo['estado_contactor']= True
+    if contactor_activo['estado_contactor']==True:
         km_contactor.on()
         led_contactor.on()
         sleep(1)
         led_contactor.off()
         sleep(1)
         recibir_desactivacion = requests.get("http://192.168.43.221:8080/desactivacion",verify=False).json() #IP cliente
-        if recibir_desactivacion.desactivacion==True:
-            contactor_activo.estado_contactor= False
+        if recibir_desactivacion['desactivacion']==True:
+            contactor_activo['estado_contactor']= False
 
     else:
         km_contactor.off()
         led_contactor.on()
     requests.post("http://192.168.43.221:8080/estado", data=json.dumps(contactor_activo))  # IP cliente
-    print("Contactor",contactor_activo.estado_contactor)
+    print("Contactor",contactor_activo)
     print("Pusador", pulsador_1.is_pressed)
     print("Mensaje Recibido", recibir_desactivacion)
     sleep(3)
